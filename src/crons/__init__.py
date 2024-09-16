e7b6327c62b5dd26a2ce52de3098ad78269ad9e5
Charles Ponti <cj@ponti.io>
2024-09-15 21:21:03 -0700

    feat: add focus vector cron

--- src/crons/__init__.py
+++ src/crons/__init__.py
@@ -1,13 +1,13 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 
-# from src.crons.add_focus_to_chroma import add_focus_to_vector_store_job
+from src.crons.add_focus_to_chroma import add_focus_to_vector_store_job
 
 scheduler = BackgroundScheduler()
 
 
 def start_scheduler():
     scheduler.start()
-    # scheduler.add_job(func=add_focus_to_vector_store_job, trigger="interval", seconds=60)
+    scheduler.add_job(func=add_focus_to_vector_store_job, trigger="interval", seconds=60)
 
 
 def shutdown_scheduler():
